7ec26fd54e917e70ba65aaa1ea6947781b3c013c
Helltar <iam@helltar.com>
2024-09-01 02:24:24 +0300

    update dependencies and versions
    upgraded ktor, kotlin, logback, and prometheus versions for better compatibility and performance. removed version variables from gradle properties to make the build file more self-contained

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,11 @@
-val kotlin_version: String by project
-val logback_version: String by project
-val prometheus_version: String by project
-
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.0.20"
     id("io.ktor.plugin") version "2.3.12"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
 }
 
 group = "com.helltar"
-version = "0.1.0"
+version = "0.1.1"
 
 application {
     mainClass.set("com.helltar.homepage.ApplicationKt")
@@ -28,13 +24,14 @@ dependencies {
     implementation("io.ktor:ktor-server-status-pages-jvm")
     implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-server-metrics-micrometer-jvm")
-    implementation("io.micrometer:micrometer-registry-prometheus:$prometheus_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-freemarker-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("io.micrometer:micrometer-registry-prometheus:1.10.5")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 
     testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
