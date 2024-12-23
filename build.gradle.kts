ed54afcd8ae3c937ec478b513e1d17b7b2cb2848
Helltar <iam@helltar.com>
2024-12-23 20:57:07 +0200

    update ktor plugin and project version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     kotlin("jvm") version "2.1.0"
-    id("io.ktor.plugin") version "3.0.1"
+    id("io.ktor.plugin") version "3.0.3"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
 }
 
 group = "com.helltar"
-version = "0.2.0"
+version = "0.2.5"
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
