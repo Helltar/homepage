ca8a7307337a3fe26ea7da030c4376657d40facb
Helltar <iam@helltar.com>
2024-12-27 01:29:16 +0200

    add custom 404 page with freemarker template

--- src/main/kotlin/com/helltar/homepage/Routing.kt
+++ src/main/kotlin/com/helltar/homepage/Routing.kt
@@ -3,6 +3,7 @@ package com.helltar.homepage
 import com.helltar.homepage.routes.index
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.freemarker.respondTemplate
 import io.ktor.server.http.content.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
@@ -20,12 +21,12 @@ fun Application.configureRouting() {
         }
 
         status(HttpStatusCode.NotFound) { call, status ->
-            call.respondText("\uD83E\uDD2D", status = status)
+            call.respondTemplate("404.ftl", null)
         }
     }
 
     routing {
         index()
-        staticFiles("/", File("data/helltar.com")) { enableAutoHeadResponse() }
+        staticFiles("/", File("data")) { enableAutoHeadResponse() }
     }
 }
---
+++ src/main/resources/templates/404.ftl
@@ -0,0 +1,10 @@
+<#import "layout.ftl" as layout />
+<@layout.header>
+
+    <div style="text-align: center">
+        <img src="/static/images/404.gif" alt="404"/>
+        <br>
+        <a href="/">back to home</a>
+    </div>
+
+</@layout.header>
