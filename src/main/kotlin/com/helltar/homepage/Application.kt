b34f0021fe8c492759edbd062a8e89558e0f1e6a
Helltar <iam@helltar.com>
2025-02-06 02:03:27 +0200

    update imports

--- src/main/kotlin/com/helltar/homepage/Application.kt
+++ src/main/kotlin/com/helltar/homepage/Application.kt
@@ -1,5 +1,9 @@
 package com.helltar.homepage
 
+import com.helltar.homepage.plugins.configureMonitoring
+import com.helltar.homepage.plugins.configureRouting
+import com.helltar.homepage.plugins.configureSerialization
+import com.helltar.homepage.plugins.configureTemplating
 import com.helltar.homepage.updaters.GitHubRepositoryUpdater
 import io.ktor.server.application.*
 
