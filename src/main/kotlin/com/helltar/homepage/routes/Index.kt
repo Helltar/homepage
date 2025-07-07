5f102e9b8a7c6905a6642753b91d4519459354ba
Helltar <iam@helltar.com>
2025-07-07 20:46:03 +0300

    add route for redirecting to ANPASIDE documentation

--- src/main/kotlin/com/helltar/homepage/routes/Index.kt
+++ src/main/kotlin/com/helltar/homepage/routes/Index.kt
@@ -11,4 +11,10 @@ fun Route.index() {
         val model = mapOf("projects" to githubRepositories())
         call.respond(FreeMarkerContent("index.ftl", model))
     }
+
+    /* https://github.com/Helltar/ANPASIDE/blob/master/app/src/main/java/com/github/helltar/anpaside/activities/MainActivity.java#L423 */
+
+    get("/mpascal/docs/") {
+        call.respondRedirect("https://github.com/Helltar/ANPASIDE?tab=readme-ov-file#anpaside")
+    }
 }
