a736fd4a8df3090683a9b2a625004b2a3bb0c83a
Helltar <iam@helltar.com>
2026-03-12 17:40:02 +0200

    replace "X-Forwarded-For" with "CF-Connecting-IP" for client IP logging in Monitoring plugin

--- src/main/kotlin/com/helltar/homepage/plugins/Monitoring.kt
+++ src/main/kotlin/com/helltar/homepage/plugins/Monitoring.kt
@@ -5,7 +5,6 @@ import com.helltar.homepage.routes.Endpoints.PATH_STATIC
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.metrics.micrometer.*
-import io.ktor.server.plugins.*
 import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
@@ -31,10 +30,10 @@ fun Application.configureMonitoring() {
             val status = call.response.status()
             val httpMethod = call.request.httpMethod.value
             val userAgent = call.request.headers["User-Agent"]
-            val remoteAddress = call.request.headers["X-Forwarded-For"] ?: call.request.origin.remoteAddress
+            val clientIp = call.request.headers["CF-Connecting-IP"] ?: call.request.headers["X-Forwarded-For"]
             val path = call.request.path()
             val time = call.processingTimeMillis()
-            "$status: $httpMethod - $path in $time ms ($remoteAddress - $userAgent)"
+            "$status: $httpMethod - $path in $time ms ($clientIp - $userAgent)"
         }
     }
 
